ab9e1080a1bf7d1a647f604ec1b053a64565a2fa
kelanim13 <lkmuelle@gmail.com>
2021-03-05 15:45:06 -0600

    more changes! :)

--- Labs/src/JavaHomework.java
+++ Labs/src/JavaHomework.java
@@ -11,6 +11,8 @@ public class JavaHomework {
 		System.out.println(lastName);
 		
 		favAnimal = "dog"; 
+		System.out.println(favAnimal);
+		
 		
 		//uninitialized variable
 		String farewell;
@@ -19,6 +21,9 @@ public class JavaHomework {
 		//integers
 		int luckyNum = 13;
 		System.out.println(luckyNum);
+		
+		int greeting = "Hello World";
+		System.out.println(greeting); 
 		
 		int myNum = 22; 
 		
@@ -29,25 +34,39 @@ public class JavaHomework {
 		
 		
 		char grade = 'A';
+		System.out.println(grade);
+		
 		char favLetter = 'K';
-		System.out.println(favLetter);
+		
 		char 1*letter = 'C';
+		System.out.println(1*letter);
 		
 		
 		double lucky = 300;
+		System.out.println(lucky);
+		
 		double randNumber = 456.2d;
 		
 		
 		boolean answer = true;
+		System.out.println(answer);
+		
 		boolean heads = false;
+		System.out.println(heads);
+		
 		boolean Tails = true;
+		
 		boolean truth = false; 
 		
 		long num = 500L;
+		System.out.println(num);
+		
 		long num2 = 234565L; 
 		
 		float pie = 3.14159F; 
+		
 		float rand Float = 45.9876F;	
+		System.out.println(rand Float);
 		
 	}
 
